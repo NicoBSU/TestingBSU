704ca1ff67d2b2beb134320d94984b51bdd2b305
Nikolai Logoshin <nikolai.logoshin@itechart-group.com>
2021-11-30 14:08:21 +0300

    fix tests

--- Lab 7/Aircompany.Tests/TestsData.cs
+++ Lab 7/Aircompany.Tests/TestsData.cs
@@ -21,12 +21,12 @@ namespace Aircompany.Tests
                     new PassengerPlane("Embraer 190", 870, 8100, 30800, 64),
                     new PassengerPlane("Sukhoi Superjet 100", 870, 11500, 50500, 140),
                     new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196),
-                    new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.Bomber),
-                    new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.Bomber),
-                    new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.Bomber),
-                    new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.Fighter),
-                    new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.Fighter),
-                    new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.Transport)
+                    new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.BOMBER),
+                    new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.BOMBER),
+                    new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.BOMBER),
+                    new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.FIGHTER),
+                    new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
+                    new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)
                 }).Returns(new PassengerPlane("Boeing-747", 980, 16100, 70500, 242));
                 
                 yield return new TestCaseData(new List<Plane>(){
@@ -35,17 +35,17 @@ namespace Aircompany.Tests
                         new PassengerPlane("Boeing-747", 980, 16100, 70500, 222),
                         new PassengerPlane("Airbus A320", 930, 11800, 65500, 222),
                         new PassengerPlane("Airbus A330", 990, 14800, 80500, 222),
-                        new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.Transport)})
+                        new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)})
                     .Returns(new PassengerPlane("Boeing-737", 900, 12000, 60500, 222));
 
                 yield return new TestCaseData(new List<Plane>()
                 {
-                    new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.Bomber),
-                    new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.Bomber),
-                    new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.Bomber),
-                    new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.Fighter),
-                    new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.Fighter),
-                    new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.Transport)
+                    new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.BOMBER),
+                    new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.BOMBER),
+                    new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.BOMBER),
+                    new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.FIGHTER),
+                    new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
+                    new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)
                 }).Returns(null);
             }
         }
@@ -63,16 +63,16 @@ namespace Aircompany.Tests
                         new PassengerPlane("Embraer 190", 870, 8100, 30800, 64),
                         new PassengerPlane("Sukhoi Superjet 100", 870, 11500, 50500, 140),
                         new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196),
-                        new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.Transport),
-                        new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.Bomber),
-                        new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.Bomber),
-                        new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.Fighter),
-                        new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.Fighter),
-                        new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.Transport)})
+                        new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.TRANSPORT),
+                        new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.BOMBER),
+                        new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.BOMBER),
+                        new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.FIGHTER),
+                        new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
+                        new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)})
                     .Returns(new List<MilitaryPlane>()
                     {
-                        new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.Transport),
-                        new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.Transport),
+                        new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.TRANSPORT),
+                        new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT),
                     });
 
                 yield return new TestCaseData(new List<Plane>(){
@@ -84,13 +84,13 @@ namespace Aircompany.Tests
                     new PassengerPlane("Embraer 190", 870, 8100, 30800, 64),
                     new PassengerPlane("Sukhoi Superjet 100", 870, 11500, 50500, 140),
                     new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196),
-                    new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.Bomber),
-                    new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.Bomber),
-                    new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.Bomber),
-                    new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.Fighter),
-                    new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.Fighter),
-                    new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.Transport)})
-                    .Returns(new List<MilitaryPlane>() {new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.Transport)});
+                    new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.BOMBER),
+                    new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.BOMBER),
+                    new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.BOMBER),
+                    new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.FIGHTER),
+                    new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
+                    new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)})
+                    .Returns(new List<MilitaryPlane>() {new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)});
                 
                 yield return new TestCaseData(new List<Plane>(){
                         new PassengerPlane("Boeing-737", 900, 12000, 60500, 222),
@@ -116,12 +116,12 @@ namespace Aircompany.Tests
                     new PassengerPlane("Embraer 190", 870, 8100, 30800, 64),
                     new PassengerPlane("Sukhoi Superjet 100", 870, 11500, 50500, 140),
                     new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196),
-                    new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.Transport),
-                    new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.Bomber),
-                    new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.Bomber),
-                    new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.Fighter),
-                    new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.Fighter),
-                    new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.Transport)
+                    new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.TRANSPORT),
+                    new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.BOMBER),
+                    new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.BOMBER),
+                    new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.FIGHTER),
+                    new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
+                    new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)
                 });
 
                 yield return new TestCaseData(new List<Plane>()

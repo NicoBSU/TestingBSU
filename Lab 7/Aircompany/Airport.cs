692a320955ea518cdc4b9383c5380ef752eb613c
Nikolai Logoshin <nikolai.logoshin@itechart-group.com>
2021-12-05 10:56:25 +0300

    recreate project

---
+++ Lab 7/Aircompany/Airport.cs
@@ -0,0 +1,66 @@
+﻿using Aircompany.Models;
+using Aircompany.Planes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aircompany
+{
+    public class Airport
+    {
+        private List<Plane> _planes;
+
+        public Airport(IEnumerable<Plane> planes)
+        {
+            _planes = planes.ToList();
+        }
+
+        public List<PassengerPlane> GetPassengersPlanes()
+        {
+            return _planes.Where(plane => plane.GetType() == typeof(PassengerPlane)).Cast<PassengerPlane>().ToList();
+        }
+
+        public List<MilitaryPlane> GetMilitaryPlanes()
+        {
+            return _planes.Where(plane => plane.GetType() == typeof(MilitaryPlane)).Cast<MilitaryPlane>().ToList();
+        }
+
+        public PassengerPlane GetPassengerPlaneWithMaxPassengersCapacity()
+        {
+            return GetPassengersPlanes().Aggregate((firstPlane, secondPlane) => firstPlane.GetPassengersCapacity() > secondPlane.GetPassengersCapacity() ? firstPlane : secondPlane);
+        }
+
+        public List<MilitaryPlane> GetTransportMilitaryPlanes()
+        {
+            return GetMilitaryPlanes().Where(plane => plane.GetType() == MilitaryType.TRANSPORT).ToList();
+        }
+
+        public Airport SortByMaxDistance()
+        {
+            return new Airport(_planes.OrderBy(plane => plane.GetMaxFlightDistance()));
+        }
+
+        public Airport SortByMaxSpeed()
+        {
+            return new Airport(_planes.OrderBy(plane => plane.GetMaxSpeed()));
+        }
+
+        public Airport SortByMaxLoadCapacity()
+        {
+            return new Airport(_planes.OrderBy(plane => plane.GetMaxLoadCapacity()));
+        }
+
+
+        public IEnumerable<Plane> GetPlanes()
+        {
+            return _planes;
+        }
+
+        public override string ToString()
+        {
+            return "Airport{" +
+                   "planes=" + string.Join(", ", _planes.Select(plane => plane.GetModel())) +
+                   '}';
+        }
+    }
+}
